d1a5dc2cbbf0a1081f52c3aa570de05e6e98d98c
bizzo <bizzo.mateus@gmail.com>
2023-10-03 16:25:13 -0300

    Add caso pydantic validator e strictint

---
+++ calculadora_03_pydantic_strict_validator.py
@@ -0,0 +1,6 @@
+from pydantic import validate_call, StrictInt
+
+class PydanticStrictValidator:
+    @validate_call
+    def soma(num1: StrictInt, num2: StrictInt) -> int:
+        return num1 + num2
