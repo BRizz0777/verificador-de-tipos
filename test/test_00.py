265049d75fcd385c8cc01da23abe720fd75ee33f
bizzo <bizzo.mateus@gmail.com>
2023-10-03 15:33:13 -0300

    Add test cases para primeira classe

---
+++ test/test_00.py
@@ -0,0 +1,24 @@
+import pytest
+from calculadora_00_notypehint import NoTypeHint
+
+calculadora = NoTypeHint()
+
+''' Tests '''
+class TestClassNoTypeHint:
+    def test_soma_inteiros(self):
+        assert calculadora.soma(2,3) == 5
+        # return 5 <int>
+
+    def test_soma_strings_num(self):
+        assert calculadora.soma("2", "3") == "23"
+        # return 23 <str>
+
+    def test_soma_string_int(self):
+        with pytest.raises(TypeError) as e_info:
+            calculadora.soma("2", 3)
+        assert 'can only concatenate str (not "int") to str' == str(e_info.value.args[0])
+        # return error
+
+    def test_soma_strings(self):
+        assert calculadora.soma("dois", "tres") == "doistres"
+    # doistres <str>
