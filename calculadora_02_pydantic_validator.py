0a0582df51b01fe87162bb95292cf4bf41469040
bizzo <bizzo.mateus@gmail.com>
2023-10-03 16:37:28 -0300

    Corrige outras classes

--- calculadora_02_pydantic_validator.py
+++ calculadora_02_pydantic_validator.py
@@ -2,5 +2,5 @@ from pydantic import validate_call
 
 class PydanticValidator:
     @validate_call
-    def soma(num1: int, num2: int) -> int:
+    def soma(self, num1: int, num2: int) -> int:
         return num1 + num2
--- calculadora_03_pydantic_strict_validator.py
+++ calculadora_03_pydantic_strict_validator.py
@@ -2,5 +2,5 @@ from pydantic import validate_call, StrictInt
 
 class PydanticStrictValidator:
     @validate_call
-    def soma(num1: StrictInt, num2: StrictInt) -> int:
+    def soma(self, num1: StrictInt, num2: StrictInt) -> int:
         return num1 + num2
